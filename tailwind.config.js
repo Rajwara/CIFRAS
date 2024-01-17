931a9497b532b288f5e111c723dedffedf15976e
Rajwara <maliksaadsultan766@gmail.com>
2024-01-17 22:15:05 +0500

    font family added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
     ],
     theme: {
         extend: {},
+
+        fontFamily: {
+            'rubik': ["Rubik", "sans-serif"],
+
+
+        }
     },
     plugins: [
         require('flowbite/plugin')
