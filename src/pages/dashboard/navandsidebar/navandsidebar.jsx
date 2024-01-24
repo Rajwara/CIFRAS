6fcc33a71a794a25d14a7b1f3792a306863b10e7
Rajwara <maliksaadsultan766@gmail.com>
2024-01-24 16:04:47 +0500

    controled the width of side bar

--- src/pages/dashboard/navandsidebar/navandsidebar.jsx
+++ src/pages/dashboard/navandsidebar/navandsidebar.jsx
@@ -58,19 +58,7 @@ const Navandsidebar = () => {
               </a>
             </div>
 
-            {/* <div className='flex gap-4 items-center justify-center '>
-   
 
-
-
-<div>
-    
-
-
-
-
-</div>
-   </div> */}
 
             <div class="flex items-center w-[30%] gap-6 ">
               <div className="w-full mr-10">
@@ -287,13 +275,7 @@ const Navandsidebar = () => {
               <p class="text-2xl text-gray-400 dark:text-gray-500"></p>
             </div>
           </div>
-          {/* <div class="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
-         <p class="text-2xl text-gray-400 dark:text-gray-500">
-            <svg class="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-            </svg>
-         </p>
-      </div> */}
+          
           <div class="grid grid-cols-2 gap-4">
             <div class=" w-full">
               <p class="text-2xl text-gray-400 dark:text-gray-500">
