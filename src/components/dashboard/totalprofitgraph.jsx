a3c581c2f111c9640444505cf828170350fcb6cd
Rajwara <maliksaadsultan766@gmail.com>
2024-01-24 16:00:39 +0500

    new dashboard graphs added

--- src/components/dashboard/totalprofitgraph.jsx
+++ src/components/dashboard/totalprofitgraph.jsx
@@ -1,16 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import ApexCharts from 'react-apexcharts';
+import React, { useState, useEffect } from "react";
+import ApexCharts from "react-apexcharts";
 
 const TotalProfitGraph = () => {
   const [chartOptions, setChartOptions] = useState({
     xaxis: {
       show: true,
-      categories: ['01 Feb', '02 Feb', '03 Feb', '04 Feb', '05 Feb', '06 Feb', '07 Feb'],
+      categories: [
+        "01 Feb",
+        "02 Feb",
+        "03 Feb",
+        "04 Feb",
+        "05 Feb",
+        "06 Feb",
+        "07 Feb",
+      ],
       labels: {
         show: true,
         style: {
-          fontFamily: 'Inter, sans-serif',
-          cssClass: 'text-xs font-normal fill-gray-500 dark:fill-gray-400',
+          fontFamily: "rubik, sans-serif",
+          cssClass: "text-xs font-normal fill-gray-500 dark:fill-gray-400",
         },
       },
       axisBorder: {
@@ -25,34 +33,34 @@ const TotalProfitGraph = () => {
       labels: {
         show: true,
         style: {
-          fontFamily: 'Inter, sans-serif',
-          cssClass: 'text-xs font-normal fill-gray-500 dark:fill-gray-400',
+          fontFamily: "rubik, sans-serif",
+          cssClass: "text-xs font-normal fill-gray-500 dark:fill-gray-400",
         },
         formatter: function (value) {
-          return '$' + value;
+          return "$" + value;
         },
       },
     },
     series: [
       {
-        name: 'Developer Edition',
+        name: "Developer Edition",
         data: [150, 141, 145, 152, 135, 125],
-        color: '#1A56DB',
+        color: "#1A56DB",
       },
       {
-        name: 'Designer Edition',
+        name: "Designer Edition",
         data: [43, 13, 65, 12, 42, 73],
-        color: '#7E3BF2',
+        color: "#7E3BF2",
       },
     ],
     chart: {
       sparkline: {
         enabled: false,
       },
-      height: '100%',
-      width: '100%',
-      type: 'area',
-      fontFamily: 'Inter, sans-serif',
+      height: "100%",
+      width: "100%",
+      type: "area",
+      fontFamily: "Inter, sans-serif",
       dropShadow: {
         enabled: false,
       },
@@ -67,12 +75,12 @@ const TotalProfitGraph = () => {
       },
     },
     fill: {
-      type: 'gradient',
+      type: "gradient",
       gradient: {
         opacityFrom: 0.55,
         opacityTo: 0,
-        shade: '#1C64F2',
-        gradientToColors: ['#1C64F2'],
+        shade: "#1C64F2",
+        gradientToColors: ["#1C64F2"],
       },
     },
     dataLabels: {
@@ -101,72 +109,55 @@ const TotalProfitGraph = () => {
     <div className="w-full bg-white rounded-lg shadow dark:bg-gray-800">
       <div className="flex justify-between p-4 md:p-6 pb-0 md:pb-0">
         <div>
-          <h5 className="leading-none text-3xl font-bold text-gray-900 dark:text-white pb-2">$12,423</h5>
-          <p className="text-base font-normal text-gray-500 dark:text-gray-400">Sales this week</p>
+          <div className=" justify-between items-center grid grid-cols-2">
+            <h5 className="leading-none text-3xl font-bold text-gray-900 dark:text-white pb-2 font-rubik">
+              $12,423
+            </h5>
+            <h5 className="leading-none text-xl font-bold  pb-2 font-rubik text-[#000] m">
+         Total profit without tax included.
+            </h5>
+              <button
+              id="dropdownDefaultButton"
+              data-dropdown-toggle="lastDaysdropdown"
+              data-dropdown-placement="bottom"
+              class="text-sm font-medium text-gray-500 dark:text-gray-400 hover:text-gray-900 text-center inline-flex items-center dark:hover:text-white font-rubik"
+              type="button"
+            >
+              Last 7 days
+              <svg
+                class="w-2.5 m-2.5 ms-1.5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 10 6"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m1 1 4 4 4-4"
+                />
+              </svg>
+            </button>
+          </div>
+          <div>
+            {/* <!-- Button --> */}
+          
+          </div>
         </div>
-        <div className="flex items-center px-2.5 py-0.5 text-base font-semibold text-green-500 dark:text-green-500 text-center">
-          23%
-          <svg className="w-3 h-3 ms-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13V1m0 0L1 5m4-4 4 4" />
-          </svg>
-        </div>
+      
       </div>
       <div id="labels-chart" class="px-2.5">
-      <ApexCharts options={chartOptions} series={chartOptions.series} type="area" height="350" />
+        <ApexCharts
+          options={chartOptions}
+          series={chartOptions.series}
+          type="area"
+          height="350"
+        />
       </div>
-      <div className="grid grid-cols-1 items-center border-gray-200 border-t dark:border-gray-700 justify-between mt-5 p-4 md:p-6 pt-0 md:pt-0">
-        <div className="flex justify-between items-center pt-5">
-          <button
-            id="dropdownDefaultButton"
-            data-dropdown-toggle="lastDaysdropdown"
-            data-dropdown-placement="bottom"
-            className="text-sm font-medium text-gray-500 dark:text-gray-400 hover:text-gray-900 text-center inline-flex items-center dark:hover:text-white"
-            type="button"
-          >
-            Last 7 days
-            <svg className="w-2.5 m-2.5 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
-            </svg>
-          </button>
-          <div id="lastDaysdropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-            <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-              <li>
-                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                  Yesterday
-                </a>
-              </li>
-              <li>
-                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                  Today
-                </a>
-              </li>
-              <li>
-                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                  Last 7 days
-                </a>
-              </li>
-              <li>
-                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                  Last 30 days
-                </a>
-              </li>
-              <li>
-                <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                  Last 90 days
-                </a>
-              </li>
-            </ul>
-          </div>
-          <a
-            href="#"
-            className="uppercase text-sm font-semibold inline-flex items-center rounded-lg text-blue-600 hover:text-blue-700 dark:hover:text-blue-500  hover:bg-gray-100 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 px-3 py-2"
-          >
-            Sales Report
-            <svg className="w-2.5 h-2.5 ms-1.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
-            </svg>
-          </a>
-        </div>
+      <div className="grid grid-cols-1 items-center border-gray-200 border-t dark:border-gray-700 justify-between mt-14 p-4 md:p-6 pt-0 md:pt-0">
+       
       </div>
     </div>
   );
