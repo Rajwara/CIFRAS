cb8c8396556c1f2a40436b9b84365e3fe3c1cb80
Rajwara <maliksaadsultan766@gmail.com>
2024-01-26 21:09:30 +0500

    cdns installed for phone field country code

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 // import img from '../cifras/src/assets/images/loginandsignup/main_screen_bg.svg'
 module.exports = {
     content: [
-        "./src/**/*.{js,jsx,ts,tsx}", "./node_modules/flowbite/**/*.js"
+        "./src/**/*.{js,jsx,ts,tsx}", "./node_modules/flowbite/**/*.js",
+        "./node_modules/tailwind-datepicker-react/dist/**/*.js", // <--- Add this line
+
 
     ],
     theme: {
