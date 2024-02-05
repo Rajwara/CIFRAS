9bfb0eaa143e0602d60c42b18e9d62b7025d9d5b
Rajwara <maliksaadsultan766@gmail.com>
2024-02-05 23:05:05 +0500

    new screen routes added

--- src/App.js
+++ src/App.js
@@ -14,8 +14,20 @@ import Clientform from './components/clients/clientform/clientform'
 import Quoteform1 from './components/quote/quoteform1';
 import Createinvoice from './pages/dashboard/Invoice/createinvoice';
 import Invoicelist from './pages/dashboard/Invoice/invoicelist'
+import Invoicedetails from './components/invoices/invoicedetails/invoicedetails';
 import Profile from './pages/dashboard/profile/profile';
 import Settings from './components/profile/settings/settings';
+import Suppliers from "./pages/dashboard/suppliers/suppliers"
+import SupplierForm from "./components/suppliers/supplierform/supplierform"
+import Reports from "./pages/dashboard/reports/reports"
+import Orders from "./pages/dashboard/orders/orders"
+// import Addorder from './components/orders/addorder';
+import OrderForm from './components/orders/orderform/orderform'
+import Vendor from "./pages/dashboard/vendors/vendors"
+import VendorForm from "./components/vendors/vendorform/vendorform"
+import CompanyData from './components/companydata/companydata'
+
+
 
 function App() {
   return (
@@ -31,7 +43,7 @@ function App() {
       <Route path='/products' element={<Products/>}/>
       <Route path='/productform' element={<Productform/>}/>
 
-      <Route path='/quote' element={<Quote/>}/>
+      <Route path='/quotes' element={<Quote/>}/>
       <Route path='/quoteform' element={<Quoteform1/>}/>
 
       <Route path='/clients' element={<Client/>}/>
@@ -39,10 +51,28 @@ function App() {
 
       <Route path='/createinvoice' element={<Createinvoice/>}/>
       <Route path='/invoicelist' element={<Invoicelist/>}/>
+      <Route path='/invoicedetails' element={<Invoicedetails/>}/>
 
       <Route path='/profile' element={<Profile/>}/>
       <Route path='/settings' element={<Settings/>}/>
 
+
+      <Route path='/suppliers' element={<Suppliers/>}/>
+      <Route path='/supplierform' element={<SupplierForm/>}/>
+
+      
+      <Route path='/reports' element={<Reports/>}/>
+
+
+      <Route path='/orders' element={<Orders/>}/>
+      <Route path='/orderform' element={<OrderForm/>}/>
+
+      <Route path='/vendor' element={<Vendor/>}/>
+      <Route path='/vendorform' element={<VendorForm/>}/>
+
+      <Route path='/companydata' element={<CompanyData/>}/>
+
+      
 
 
 
