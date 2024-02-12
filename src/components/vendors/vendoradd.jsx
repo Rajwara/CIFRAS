cc41164de5a3958f346630758b0a64f38692e8ac
Rajwara <maliksaadsultan766@gmail.com>
2024-02-13 00:33:59 +0500

    font family added to vendor screens

--- src/components/vendors/vendoradd.jsx
+++ src/components/vendors/vendoradd.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import Plus from "../../assets/images/dashboard/plusgoodmorning.svg";
+import Totalrevenuecardforotherpage from "../cards/totalrevenuecard";
+import Numberofordercardsforotherpage from '../cards/numberofordercard';
 
 const Vendoradd = () => {
   return (
     <div className='flex justify-between border border-[#ebebeb] bg-white   py-8 px-10 items-center'>
       <div className='left flex flex-col gap-2 relative'>
-          <h2 className='font-rubik font-bold text-[#404040] text-4xl leading-9'>Vendors</h2>
+          <h2 className='font-lexend font-bold text-[#404040] text-4xl leading-9'>Vendors</h2>
       </div>
+
+      <div className='middle flex flex-row gap-6 justify-center'>
+        <Numberofordercardsforotherpage/>
+        <Totalrevenuecardforotherpage/>
+      </div>
+
       <div className='right'>
         <a href='/vendorform'>
-            <button className='flex py-[10px] px-[16px] bg-[#634af9] text-xl font-rubik text-white rounded items-center gap-2'>
+            <button className='flex py-[10px] px-[16px] bg-[#634af9] text-xl font-inter text-white rounded items-center gap-2'>
             <img src={Plus} className='w-6 h-6  ' alt=''/>
             Add Vendor
             </button>
