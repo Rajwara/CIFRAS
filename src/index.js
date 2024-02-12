89dbbe8ef63f889653c01e94cc78b215b265fe3d
Rajwara <maliksaadsultan766@gmail.com>
2024-02-13 00:32:06 +0500

    presist and store added for storing values and data

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { Store ,persistor} from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={Store}>
     <BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
     <App />
+    </PersistGate>
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
