8553b8deb1f6711c573642433e2acaccfe12f3a1
rana asad <asadullah559921@gmail.com>
2024-02-23 19:30:11 +0500

    change some invoice page styling and purchase order page change styling

--- src/components/invoices/invoicelist/invoicelistbtnbar.jsx
+++ src/components/invoices/invoicelist/invoicelistbtnbar.jsx
@@ -7,8 +7,8 @@ import Totalrevenuecardforotherpage from "../../cards/totalrevenuecard";
 
 const invoicelistbtnbar = () => {
   return (
-    <div className='flex flex-col lg:flex-row justify-between  bg-white  rounded py-6 px-8 items-center'>
-      <div className=' flex lg:w-[18%] xl:w-[20%] flex-col gap-2 relative'>
+    <div className='flex flex-col lg:flex-row justify-between  bg-white border border-[#ebebeb]  rounded py-6 px-8 items-center'>
+      <div className=' flex lg:w-[30%] xl:w-[25%] flex-col gap-2 relative'>
         <div className='flex gap-2'>
           <h4 className='Font-lexend text-2xl mt-[-25px] font-extrabold leading-tight text-[#404040] '>
             Create Invoice
@@ -21,7 +21,7 @@ const invoicelistbtnbar = () => {
         <Totalrevenuecardforotherpage />
       </div>
 
-      <div className='right flex flex-col xl:flex-row justify-center items-center gap-4 mt-4 lg:mt-0 lg:w-[20%] xl:w-[20%]'>
+      <div className='right flex flex-col xl:flex-row justify-center items-center gap-4 mt-4 lg:mt-0 lg:w-[20%] '>
         {/* <button className='flex py-[10px] px-[16px] bg-[#634af9] text-white rounded items-center gap-4 text-sm font-semibold'>
           <img src={ImportIcon} className='w-3.5 h-3.5  ' alt='' />
           Import
--- src/components/purchaseorder/addpurchaseorder.jsx
+++ src/components/purchaseorder/addpurchaseorder.jsx
@@ -6,7 +6,7 @@ import Numberofpurchaseorder from "../cards/purchaseorders/numberofpurchaseorder
 const Addpurchaseorder = () => {
   return (
     <div className='flex flex-col lg:flex-row justify-between border border-[#ebebeb] bg-white   py-8 px-10 items-center'>
-      <div className='left flex lg:w-[10%] xl:w-[20%] flex-col gap-2 relative'>
+      <div className='left flex lg:w-[10%] xl:w-[31%] flex-col gap-2 relative'>
         <h2 className='font-lexend font-semibold text-[#404040] text-2xl leading-9'>
           Purchase Order
         </h2>
@@ -15,7 +15,7 @@ const Addpurchaseorder = () => {
         <Numberofpurchaseorder />
         <Totalrevenuecardforotherpage />
       </div>
-      <div className='right lg:w-[20%] xl:w-[20%] mt-6 lg:mt-0'>
+      <div className='right lg:w-[20%] xl:w-[24%] mt-6 lg:mt-0'>
         <a href='/purchaseform'>
           <button className='flex py-[10px] px-[8px] bg-[#634af9] text-sm font-lexend text-white rounded items-center gap-2'>
             <img src={Plus} className='w-3.5 h-3.5  ' alt='' />
